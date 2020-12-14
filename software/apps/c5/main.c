babdc7d2a8011f52da5aae87f0538ce3c1391653
Katelyn Biesiadecki <kbiesiadecki141@gmail.com>
2020-12-13 21:14:20 -0800

    Added PWM to c5

--- software/apps/c5/main.c
+++ software/apps/c5/main.c
@@ -17,8 +17,74 @@
 #include "nrf_log_default_backends.h"
 #include "nrf_pwr_mgmt.h"
 #include "nrf_drv_spi.h"
+#include "nrf_drv_pwm.h"
 
 #include "c5Functions.h"
+
+// We want to use the PWM drivers, not the PWM library and APP_PWM_INSTANCE.
+nrf_drv_pwm_t m_pwm0 = NRF_DRV_PWM_INSTANCE(0);
+nrf_drv_pwm_t m_pwm1 = NRF_DRV_PWM_INSTANCE(1);
+nrf_drv_pwm_t m_pwm2 = NRF_DRV_PWM_INSTANCE(2);
+
+// This is for tracking PWM instances being used, so we can unintialize only
+// the relevant ones when switching from one demo to another.
+#define USED_PWM(idx) (1UL << idx)
+uint8_t m_used = 0;
+
+// define the pwm time period for pwm wave
+// 5000, 20000 is another nice-sounding frequency
+#define FREQ_IN_US 20000
+
+
+// Declare variables holding PWM sequence values. In this example only one channel is used 
+nrf_pwm_values_individual_t seq_values[] = {0, 0, 0, 0};
+
+nrf_pwm_sequence_t const seq =
+{
+    .values.p_individual = seq_values,
+    .length          = NRF_PWM_VALUES_LENGTH(seq_values),
+    .repeats         = 0,
+    .end_delay       = 0
+};
+// Set duty cycle between 0 and 100%
+void pwm_update_duty_cycle(uint8_t duty_cycle, bool direction)
+{
+    // Move forward by default with REAR_RIGHT_A and REAR_LEFT_B
+    if (duty_cycle >= 100) {
+            seq_values->channel_0 = 100;
+            seq_values->channel_1 = 100;
+            seq_values->channel_2 = 100;
+            seq_values->channel_3 = 100;
+        } else {
+            seq_values->channel_0 = duty_cycle;
+            seq_values->channel_1 = duty_cycle;
+            seq_values->channel_2 = duty_cycle;
+            seq_values->channel_3 = duty_cycle;
+        }
+
+    // potentially have different sequences for backwards/forwards
+    nrf_drv_pwm_simple_playback(&m_pwm0, &seq, 3, NRF_DRV_PWM_FLAG_LOOP);
+    nrf_drv_pwm_simple_playback(&m_pwm1, &seq, 3, NRF_DRV_PWM_FLAG_LOOP);
+    nrf_drv_pwm_simple_playback(&m_pwm2, &seq, 3, NRF_DRV_PWM_FLAG_LOOP);
+}
+void pwm_init(nrf_drv_pwm_t * m_pwm, int one, int two, int three, int four)
+{
+    nrf_drv_pwm_config_t const config =
+    {
+        .output_pins = { one, two, three, four }, // channel 1-4
+        .irq_priority = APP_IRQ_PRIORITY_LOWEST,
+        .base_clock   = NRF_PWM_CLK_1MHz,
+        .count_mode   = NRF_PWM_MODE_UP,
+        .top_value    = 100,
+        .load_mode    = NRF_PWM_LOAD_INDIVIDUAL,
+        .step_mode    = NRF_PWM_STEP_AUTO
+    };
+
+    // Init PWM without error handler
+    printf("init %02x \n", m_pwm);
+    APP_ERROR_CHECK(nrf_drv_pwm_init(m_pwm, &config, NULL));
+}
+
 
 // I2C manager
 // NRF_TWI_MNGR_DEF(twi_mngr_instance, 5, 0);
@@ -32,6 +98,23 @@ int main(void) {
   APP_ERROR_CHECK(error_code);
   NRF_LOG_DEFAULT_BACKENDS_INIT();
   printf("Log initialized!\n");
+
+  // Start clock for accurate frequencies
+  NRF_CLOCK->TASKS_HFCLKSTART = 1;
+  // Wait for clock to start
+  while(NRF_CLOCK->EVENTS_HFCLKSTARTED == 0) ;
+
+      pwm_init(&m_pwm0, FRONT_LEFT_A, FRONT_LEFT_B,
+                      FL_A2 | NRF_DRV_PWM_PIN_INVERTED,
+                      FL_B2 | NRF_DRV_PWM_PIN_INVERTED);
+
+    pwm_init(&m_pwm1, FRONT_RIGHT_A, FRONT_RIGHT_B,
+                      FR_A2 | NRF_DRV_PWM_PIN_INVERTED,
+                      FR_B2 | NRF_DRV_PWM_PIN_INVERTED);
+
+    pwm_init(&m_pwm2, REAR_RIGHT_A, REAR_LEFT_B,
+                      RR_B1 | NRF_DRV_PWM_PIN_INVERTED,
+                      RL_A2 | NRF_DRV_PWM_PIN_INVERTED);
 
   float front_close = 0.2;
   float side_close = 1;//0.2;
@@ -69,6 +152,7 @@ int main(void) {
     // Note: removing this delay will make responses quicker, but will result
     //  in printf's in this loop breaking JTAG
     nrf_delay_ms(1);
+    pwm_update_duty_cycle(0, false);
 
     // handle states
     switch(state) {
